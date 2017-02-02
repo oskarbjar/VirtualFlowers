d70882c95be8d1cc3019619d48e5e06219b06ca1
Steini <Steini@STEINI-BORDPC>
2017-02-02 20:42:13 +0000

    - Simplified the GetMapStatistic function, by running FixTeamMatches, which sets "our" always as Team1 in the list. - Small changes to Home/Index, text changes and link to compare and teams.

--- CsScraperMVC/Data/dataWorker.cs
+++ CsScraperMVC/Data/dataWorker.cs
@@ -115,43 +115,86 @@ namespace CsScraperMVC.Data
             
             return result;
         }
-           
+
         public List<MapStatisticModel> GetMapStatistics(int TeamId, DateTime dFrom, DateTime dTo)
         {
             var result = new List<MapStatisticModel>();
 
             var matches = _db.Match
                 .Where(p => (p.Team1Id == TeamId || p.Team2Id == TeamId)
-                && (p.Date >= dFrom.Date && p.Date <= dTo.Date))
-                .GroupBy(k => k.Map).ToList();
-            
-            result = matches.Select(n => new MapStatisticModel
+                && (p.Date >= dFrom.Date && p.Date <= dTo.Date)).ToList();
+
+            // Fix matches so that "our" team is always Team1, to simplify later query
+            var fixedMatches = FixTeamMatches(matches, TeamId);
+
+            // Group list by maps.
+            var groupedbymaps = fixedMatches.GroupBy(k => k.Map).ToList();
+
+            result = groupedbymaps.Select(n => new MapStatisticModel
             {
                 Map = n.Key,
-                TotalWins = n.Count(p => (p.Team1Id == TeamId) ? p.ResultT1 > p.ResultT2 : p.ResultT2 > p.ResultT1),
-                TotalLosses = n.Count(p => (p.Team1Id == TeamId) ? p.ResultT2 > p.ResultT1 : p.ResultT1 > p.ResultT2),
-                WinPercent = Math.Round(n.Count(p => (p.Team1Id == TeamId) ? p.ResultT1 > p.ResultT2 : p.ResultT2 > p.ResultT1) / (double)n.Count() * 100, 1),
+                TotalWins = n.Count(p => p.ResultT1 > p.ResultT2),
+                TotalLosses = n.Count(p => p.ResultT2 > p.ResultT1),
+                WinPercent = Math.Round(n.Count(p => p.ResultT1 > p.ResultT2) / (double)n.Count() * 100, 1),
                 // *** Get Average Win rounds ***
-                AverageWinRounds = Math.Round(n.Sum(p => (p.Team1Id == TeamId) ? p.ResultT1 : p.ResultT2) / (double)n.Count(), 1),
+                AverageWinRounds = Math.Round(n.Sum(p => p.ResultT1) / (double)n.Count(), 1),
                 // *** Get Average Loss rounds ***
-                AverageLossRounds = Math.Round(n.Sum(p => (p.Team1Id == TeamId) ? p.ResultT2 : p.ResultT1) / (double)n.Count(), 1),
+                AverageLossRounds = Math.Round(n.Sum(p => p.ResultT2) / (double)n.Count(), 1),
                 // *** Get Average Win rounds when team has won ***
-                AverageWinRoundsWhenWin = Math.Round(n.Where(p => (p.Team1Id == TeamId) ? p.ResultT1 > p.ResultT2 : p.ResultT2 > p.ResultT1) // Where team won
-                    .Sum(p => (p.Team1Id == TeamId) ? p.ResultT1 : p.ResultT2) / // Sum team rounds
-                    (double)n.Count(p => (p.Team1Id == TeamId) ? p.ResultT1 > p.ResultT2 : p.ResultT2 > p.ResultT1), 1), // Divided total games we won
+                AverageWinRoundsWhenWin = Math.Round(n.Where(p => p.ResultT1 > p.ResultT2) // Where team won
+                    .Sum(p => p.ResultT1) / // Sum team rounds
+                    (double)n.Count(p => p.ResultT1 > p.ResultT2), 1), // Divided total games we won
                 // *** Get Average Loss rounds when team has won ***
-                AverageLossRoundsWhenWin = Math.Round(n.Where(p => (p.Team1Id == TeamId) ? p.ResultT1 > p.ResultT2 : p.ResultT2 > p.ResultT1) // Where team won
-                    .Sum(p => (p.Team1Id == TeamId) ? p.ResultT2 : p.ResultT1) / // Sum opponents rounds
-                    (double)n.Count(p => (p.Team1Id == TeamId) ? p.ResultT1 > p.ResultT2 : p.ResultT2 > p.ResultT1), 1), // Divided total games we won
+                AverageLossRoundsWhenWin = Math.Round(n.Where(p => p.ResultT1 > p.ResultT2) // Where team won
+                    .Sum(p => p.ResultT2) / // Sum opponents rounds
+                    (double)n.Count(p => p.ResultT1 > p.ResultT2), 1), // Divided total games we won
                 // *** Get Average Win rounds when team has lost ***
-                AverageWinRoundsWhenLoss = Math.Round(n.Where(p => (p.Team1Id == TeamId) ? p.ResultT1 < p.ResultT2 : p.ResultT2 < p.ResultT1) // Where team lost
-                    .Sum(p => (p.Team1Id == TeamId) ? p.ResultT1 : p.ResultT2) / // Sum team rounds
-                    (double)n.Count(p => (p.Team1Id == TeamId) ? p.ResultT1 < p.ResultT2 : p.ResultT2 < p.ResultT1), 1), // Divided total games we lost
+                AverageWinRoundsWhenLoss = Math.Round(n.Where(p => p.ResultT1 < p.ResultT2) // Where team lost
+                    .Sum(p => p.ResultT1) / // Sum team rounds
+                    (double)n.Count(p => p.ResultT1 < p.ResultT2), 1), // Divided total games we lost
                 // *** Get Average Loss rounds when team has lost ***
-                AverageLossRoundsWhenLoss = Math.Round(n.Where(p => (p.Team1Id == TeamId) ? p.ResultT1 < p.ResultT2 : p.ResultT2 < p.ResultT1) // Where team lost
-                    .Sum(p => (p.Team1Id == TeamId) ? p.ResultT2 : p.ResultT1) / // Sum opponents rounds
-                    (double)n.Count(p => (p.Team1Id == TeamId) ? p.ResultT1 < p.ResultT2 : p.ResultT2 < p.ResultT1), 1) // Divided total games we lost
+                AverageLossRoundsWhenLoss = Math.Round(n.Where(p => p.ResultT1 < p.ResultT2) // Where team lost
+                    .Sum(p => p.ResultT2) / // Sum opponents rounds
+                    (double)n.Count(p => p.ResultT1 < p.ResultT2), 1) // Divided total games we lost
             }).OrderByDescending(n => n.WinPercent).ToList();
+
+            return result;
+        }
+
+        /// <summary>
+        /// Take matches query and set "our" (TeamId) team always as Team1
+        /// </summary>
+        /// <param name="matches"></param>
+        /// <param name="TeamId"></param>
+        /// <returns></returns>
+        public List<Match> FixTeamMatches(List<Match> matches, int TeamId)
+        {
+            var result = matches.Select(n => new Match
+            {
+                Id = n.Id,
+                MatchId = n.MatchId,
+                Date = n.Date,
+                Map = n.Map,
+                Event = n.Event,
+                ResultT1 = n.Team1Id == TeamId ? n.ResultT1 : n.ResultT2, // 
+                ResultT2 = n.Team1Id == TeamId ? n.ResultT2 : n.ResultT1,
+                FirstRound1HWinTeamId = n.FirstRound1HWinTeamId,
+                FirstRound1HWinCtTerr = n.FirstRound1HWinCtTerr,
+                FirstRound2HWinTeamId = n.FirstRound2HWinTeamId,
+                FirstRound2HWinCtTerr = n.FirstRound2HWinCtTerr,
+                Team1Id = n.Team1Id == TeamId ? n.Team1Id : n.Team2Id,
+                T1Player1Id = n.Team1Id == TeamId ? n.T1Player1Id : n.T2Player1Id,
+                T1Player2Id = n.Team1Id == TeamId ? n.T1Player2Id : n.T2Player2Id,
+                T1Player3Id = n.Team1Id == TeamId ? n.T1Player3Id : n.T2Player3Id,
+                T1Player4Id = n.Team1Id == TeamId ? n.T1Player4Id : n.T2Player4Id,
+                T1Player5Id = n.Team1Id == TeamId ? n.T1Player5Id : n.T2Player5Id,
+                Team2Id = n.Team1Id == TeamId ? n.Team1Id : n.Team2Id,
+                T2Player1Id = n.Team1Id == TeamId ? n.T2Player1Id : n.T1Player1Id,
+                T2Player2Id = n.Team1Id == TeamId ? n.T2Player2Id : n.T1Player2Id,
+                T2Player3Id = n.Team1Id == TeamId ? n.T2Player3Id : n.T1Player3Id,
+                T2Player4Id = n.Team1Id == TeamId ? n.T2Player4Id : n.T1Player4Id,
+                T2Player5Id = n.Team1Id == TeamId ? n.T2Player5Id : n.T1Player5Id,
+            }).ToList();
 
             return result;
         }
