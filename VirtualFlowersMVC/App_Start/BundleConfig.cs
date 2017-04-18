45f599f851110da42a4d131980c6121b6cc058f4
Steini <Steini@STEINI-BORDPC>
2017-04-18 18:55:46 +0000

    fix attempt

--- VirtualFlowersMVC/App_Start/BundleConfig.cs
+++ VirtualFlowersMVC/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace VirtualFlowersMVC
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-3.1.1.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
