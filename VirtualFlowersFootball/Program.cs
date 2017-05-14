424299c089ada284871122d6f90d148920c9a08b
oskarbjar <oskarbjar@gmail.com>
2017-05-14 21:46:41 +0000

    Adding football tips from betting page

--- VirtualFlowersFootball/Program.cs
+++ VirtualFlowersFootball/Program.cs
@@ -1,15 +1,36 @@
-﻿using System;
+﻿using HtmlAgilityPack;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace VirtualFlowersFootball
 {
-    class Program
+    public class Program
     {
+
+        readonly HtmlWeb HWeb = new HtmlAgilityPack.HtmlWeb();
         static void Main(string[] args)
         {
+           // GetBettingTips();
         }
+
+        public List<string>  GetBettingTips()
+        {
+            var contentList = new List<string>();
+            string url = "https://www.thepunterspage.com/category/tips/";
+
+            var matchesHtml = HWeb.Load(url);
+            var xxx = $"//div[@class='post_content']";
+            var newstring = "";
+            var selection = matchesHtml.DocumentNode.SelectNodes(xxx);        
+            for (int i = 0; i < selection.Count; i++)
+            {
+                var bla = selection[i].InnerHtml;
+                string results = Regex.Replace(bla, @"\n\n?|\n", newstring);
+                contentList.Add(results);
+            }
+
+            return contentList;
+        }
+       
     }
 }
